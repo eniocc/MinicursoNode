6f70a05f77f38c347876b876a50418a61f2e1937
Ênio Viana <eniocc@gmail.com>
2020-02-04 20:31:31 -0300

    Atualiza API com Router Express

--- node-exemplos/node-express/index.js
+++ node-exemplos/node-express/index.js
@@ -5,6 +5,8 @@ const http = require('http')
 const express = require('express')
 const porta = 3000
 
+const rotasPizzas = require('./rotas/pizzas')
+
 const app = express()
 
 app.use(morgan('dev'))
@@ -13,30 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.all('/pizzas', (req, res, next) => {
-    res.status(200)
-    res.append('Content-Type', 'text/plain')
-    next()
-})
-
-app.get('/pizzas', (req, res) => {
-    res.end('enviando todas as pizzas!')
-})
-
-app.post('/pizzas', (req, res) => {
-    const p = req.body
-    res.end(`adicionando a pizza: ${p.name}, ${p.description}`)
-})
-
-app.put('/pizzas', (req, res) => {
-    res.status(405)
-    res.append('Allow', ['GET', 'POST', 'DELETE'])
-    res.end('operação PUT não é suportada em /pizzas')
-})
-
-app.delete('/pizzas', (req, res) => {
-    res.end('deletando todas as pizzas!')
-})
+app.use('/pizzas', rotasPizzas)
 
 const servidor = http.createServer(app)
 servidor.listen(porta, () => {

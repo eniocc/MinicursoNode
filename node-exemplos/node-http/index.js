8c5d20790e2a8756d1ca9fd5c953e23094b128d3
Ênio Viana <eniocc@gmail.com>
2020-02-04 20:17:32 -0300

    Adiciona servidor de arquivos

--- node-exemplos/node-http/index.js
+++ node-exemplos/node-http/index.js
@@ -1,12 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+
 const http = require('http')
 const hostname = 'localhost'
 const port = 3000
+
 const server = http.createServer((req, res) => {
-    console.log('cabeçalhos da requisição:', req.headers)
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<html><body><h1>Hello, World!</h1></body></html>')
+    console.log('requisição:', req.method, req.url)
+    console.log('cabeçalhos:', req.headers)
+    if (req.method != 'GET') {
+        res.statusCode = 405
+        res.setHeader('Content-Type', 'text/html')
+        res.setHeader('Allow', 'GET')
+        return res.end(`<h1>Error 405: ${req.method} is not supported</h1>`)
+    }
+    let targetResource
+    if (req.url == '/')
+        targetResource = '/index.html'
+    else
+        targetResource = req.url
+    const filePath = path.resolve('./public' + targetResource)
+    fs.exists(filePath, (exists) => {
+        if (!exists) {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/html');
+            return res.end(`<h1>Error 404: ${req.url} not found</h1>`)
+        }
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        fs.createReadStream(filePath).pipe(res)
+    })
 })
+
 server.listen(port, hostname, () => {
     console.log(`servidor escutando em http://${hostname}:${port}/`)
 })

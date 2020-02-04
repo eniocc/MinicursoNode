4f009a823711f07757981b85bbc67545821c9e6d
Ênio Viana <eniocc@gmail.com>
2020-02-04 20:14:27 -0300

    Adiciona servidor HTTP

---
+++ node-exemplos/node-http/index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const hostname = 'localhost'
+const port = 3000
+const server = http.createServer((req, res) => {
+    console.log('cabeçalhos da requisição:', req.headers)
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><body><h1>Hello, World!</h1></body></html>')
+})
+server.listen(port, hostname, () => {
+    console.log(`servidor escutando em http://${hostname}:${port}/`)
+})
